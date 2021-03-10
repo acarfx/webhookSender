1d6f4747970606ef0324d66a16fb1932480a2108
acar <77089894+acarfx@users.noreply.github.com>
2021-03-10 08:54:25 +0300

    Create Reference.js

---
+++ Reference.js
@@ -0,0 +1,24 @@
+const { GuildMember, Guild, TextChannel } = require("discord.js");
+const Webhooklar = {};
+const client = global.client;
+ /**
+ * @param {String} id
+ * @returns {GuildMember} 
+ */
+TextChannel.prototype.wsend = async function (content, options) {
+    if (Webhooklar[this.id]) return (await Webhooklar[this.id].send(content, options));
+    let entegrasyonlar = await this.fetchWebhooks();
+    let webh = entegrasyonlar.find(e => e.name == client.user.username),
+        result;
+    if (!webh) {
+        webh = await this.createWebhook(client.user.username, {
+            avatar: client.user.avatarURL()
+        });
+        Webhooklar[this.id] = webh;
+        result = await webh.send(content, options);
+    } else {
+        Webhooklar[this.id] = webh;
+        result = await webh.send(content, options);
+    }
+    return result;
+};
